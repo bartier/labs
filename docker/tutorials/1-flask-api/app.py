ad0c3dc827e97c1143ee9ab30532fef809cf69cd
Vitor Anjos <bartier@users.noreply.github.com>
2025-04-10 10:47:38 -0300

    update

--- docker/tutorials/1-flask-api/app.py
+++ docker/tutorials/1-flask-api/app.py
@@ -8,4 +8,4 @@ def get_random_number():
     return jsonify({"number": random.randint(1, 100)})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
